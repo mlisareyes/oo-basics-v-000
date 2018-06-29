8bef6959b6d765743266734f3b3b5ecfcf7d47d5
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-29 20:53:51 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -24,4 +24,18 @@ class Shoe
     @size
   end
 
+  def material=(material)
+    @material = material
+  end
+
+  def material
+    @material
+  end
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def condition
+    @condition
 end
